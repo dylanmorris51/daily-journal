70e2d4b2aa80056b818adbeff6fa7e999c0b33ed
Dylan Morris <dylanrobertmorris@gmail.com>
2021-01-15 14:12:02 -0600

    tried styling

--- scripts/JournalEntry.js
+++ scripts/JournalEntry.js
@@ -2,10 +2,12 @@ export const JournalEntryComponent = (entry) => {
     return `
         <section id="entry--${entry.id}" class="journalEntry">
             <hr>
-            <div class="entry__date">${entry.date}</div>
-            <div class="entry__concepts">${entry.concept}</div>
+            <div class="pastEntry__top">
+                <div class="entry__date past__block">${entry.date}</div>
+                <div class="entry__concepts past__block">${entry.concept}</div>
+                <div class="entry__mood past__block">${entry.mood}</div>
+            </div>
             <div class="entry__entry">${entry.entry}</div>
-            <div class="entry__mood>${entry.mood}</div>
             <div class="entry__actions">
                 <input type="submit" value="Edit" class="entry__edit">
                 <input type="submit" value="Delete" class="entry__delete">
