935c4a3535e07c5c9370656c1696fbb86497eeec
Dylan Morris <dylanrobertmorris@gmail.com>
2021-02-01 13:46:34 -0600

    rendered Journal Form to DOM as it's own component

---
+++ scripts/JournalForm.js
@@ -0,0 +1,51 @@
+const eventHub = document.querySelector(".container")
+const contentTarget = document.querySelector(".newJournal")
+
+
+const render = () => {
+    contentTarget.innerHTML = `
+    <h2>Daily Journal</h2>
+    <section class="journalFormInput">
+    <form action="">
+    <!-- <fieldset> -->
+    <!-- <fieldset> -->
+        <label for="journalDate">Date of Entry:</label>
+        <input type="date" name="journalDate" id="journalDate">
+    <!-- </fieldset> -->
+    <!-- <fieldset>   -->
+        <label for="journalTitle">Title:</label>
+        <input type="text" name="journalTitle" id="journalTitle">
+    <!-- </fieldset>   -->
+    <!-- <fieldset> -->
+        <label for="journalConcepts">Concepts:</label>
+        <input type="text" name="journalConcepts" id="journalConcepts">
+    <!-- </fieldset> -->
+    <!-- <fieldset> -->
+        <label for="journalEntry">Journal Entry:</label>
+        <textarea rows="4" name="jorunalEntry" id="jorunalEntry"></textarea>
+    <!-- </fieldset> -->
+    <!-- <fieldset> -->
+        <label for="journalMood">Mood:</label>
+        <select type="journalMood" name="journalMood" id="journalMood">
+            <option value="Fine">Fine</option>
+            <option value="Excited">Excited</option>
+            <option value="Proud">Proud</option>
+            <option value="Determined">Determined</option>
+            <option value="Overwhelmed">Overwhelmed</option>
+            <option value="Frusturated">Frusturated</option>
+        </select>
+    <!-- </fieldset> -->
+    <!-- </fieldset> -->
+    
+    <div id="submitButton">
+        <input type="submit" value="Record Journal Entry" name="journalButton" id="journalButton">
+    </div>
+    
+</form>
+    </section>
+    `
+}
+
+export const JournalForm = () => {
+    render()
+}
